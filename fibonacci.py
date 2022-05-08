f1462e98a022cfbf8f0a6f0d8da13c3f865c373e
Gramsci <gramsciHC@gmail.com>
2022-05-08 11:47:48 -0400

    Added description to the fibonacci problem

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,5 @@
-# The Fibonacci numbers are the numbers in the following integer sequence.
+# The Fibonacci sequence is devined as follows: the firts number of the sequence is 0,
+# the second number is 1, and the nth number is the sum of the (n-1)th and (n-2)th numbers.
+# Write a function that takes in an integer n and returns the nth Fibonacci number.
 # ex. -> 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
 # Fn = Fn-1 + Fn-2
-
-
